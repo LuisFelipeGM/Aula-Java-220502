f7d99e360a6e77566644201d2b2cab0eb3fca613
LuisFelipeGM <rm94051@fiap.com.br>
2022-05-07 22:42:10 -0300

    Corrigi a atividade de Data e String

--- src/br/com/fiap/PraticandoData.java
+++ src/br/com/fiap/PraticandoData.java
@@ -2,33 +2,38 @@ package br.com.fiap;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
 
 import javax.swing.JOptionPane;
 
 public class PraticandoData {
 	public static void main(String[] args) {
-		String aux;
-		LocalDate data1, data2;
-		
-		DateTimeFormatter dft = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		DateTimeFormatter dfta = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		
-		aux = JOptionPane.showInputDialog("Digite a data inicial: ");
-		data1 = LocalDate.parse(aux);
-		String dataAm1 = data1.format(dfta);
-		String dataFormatada1 = data1.format(dft);
-		
-		
-		aux = JOptionPane.showInputDialog("Digite a data final: ");
-		data2 = LocalDate.parse(aux);
-		String dataAm2 = data1.format(dfta);
-		String dataFormatada2 = data1.format(dft);
-		
-		Period periodo = Period.between(data1, data2);
-		JOptionPane.showMessageDialog(null, "Data Inicial: " + dataFormatada1 +
-				"\nData Final: " + dataFormatada2 + "\n Entre as datas fornecidas existem"
-						+ periodo.getYears() + " ano(s), " + periodo.getMonths()
-						+ " mes(es) e " + periodo.getDays() + " dia(s)!");
+		String data1, data2, formatoUSA;
+		LocalDate datai, dataf;
+		try {
+			data1 = JOptionPane.showInputDialog("Digite uma data Inicial");
+			data2 = JOptionPane.showInputDialog("Digite uma data Final");
+			// 0 1 2 3 4 5 6 7 8 9
+			// 0 8 / 0 1 / 1 9 9 0
+			formatoUSA = data1.substring(6, 10); // 1990
+			formatoUSA += "-" + data1.substring(3, 5); // 1990-01
+			formatoUSA += "-" + data1.substring(0, 2); // 1990-01-08
+			datai = LocalDate.parse(formatoUSA);
+			
+			formatoUSA = data2.substring(6, 10);
+			formatoUSA += "-" + data2.substring(3, 5);
+			formatoUSA += "-" + data2.substring(0, 2);
+			dataf = LocalDate.parse(formatoUSA);
+			
+			Period periodo = Period.between(datai, dataf);
+			
+			JOptionPane.showMessageDialog(null,
+					"Data inicial: " + data1 + "\nData final: " + data2 +
+					"\nEntre as datas fornecidas existem " + periodo.getYears()
+					+ " ano(s), " + periodo.getMonths() + " mes(es) e "
+					+ periodo.getDays() + " dia(s)!");
+			
+		} catch (Exception e) {
+			JOptionPane.showInternalMessageDialog(null, e.getMessage());
+		}
 	}
 }
